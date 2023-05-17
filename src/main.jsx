b2b49ab80debdb8222b3363866341bd96b7ac435
aalnoman10 <aalnomanm10@gmail.com>
2023-05-18 01:25:06 +0600

    navBar update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "./index.css"
 import router from './routes/Route.jsx'
 import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <div className="container">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
 )
--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/toy.png';
+import { useState } from 'react';
 
 const NavigationBar = () => {
+    const [user, setUser] = useState('v')
+
+    const li = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/'>All Toys</Link></li>
+        {user && <>
+            <li><Link to='/'>My Toys</Link></li>
+            <li><Link to='/'>Add A Toy</Link></li>
+        </>}
+        <li><Link to='/'>Blogs</Link></li>
+    </>
+
     return (
-        <div>
-            Navigation
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {li}
+                    </ul>
+                </div>
+                <div className='flex items-center pl-2'>
+                    <img src={logo} className='w-14' alt="" />
+                    <span className="btn btn-ghost normal-case text-xl p-1">Animal Toy</span>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {li}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {user ? <img src={logo} className='w-14' title={user?.displayName} alt="User profile picture" /> : <Link to='login' className='btn btn-ghost normal-case text-xl'>Login</Link>
+                }
+            </div>
         </div>
     );
 };
